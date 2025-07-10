016dc0c757ed4d16ea4d1fcac68351497ba261b7
Akhil2665 <akhileee.16@gmail.com>
2025-07-10 23:40:44 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const shorteningUrl = require("./routes/urlRoutes.js");
+const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
 const rateLimit = require("express-rate-limit");
+
+dotenv.config({ path: "../config/.env" });
 
 const limiter = rateLimit({
   windowMs: 30 * 60 * 1000,
@@ -18,7 +21,7 @@ app.use(limiter);
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_DEPLOYED_URL || process.env.FRONTEND_LOCAL_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
