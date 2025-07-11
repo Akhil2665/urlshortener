6f44cca7222b18ffcb1e6993daddb83019acec4c
Akhil2665 <akhileee.16@gmail.com>
2025-07-11 08:17:15 +0530

    revert back to prev version

--- app.js
+++ app.js
@@ -2,33 +2,22 @@ const express = require("express");
 const shorteningUrl = require("./routes/urlRoutes.js");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
-const { RateLimiterRedis } = require("rate-limiter-flexible");
-const Redis = require("ioredis");
-const redisClient = new Redis();
-
 const app = express();
+const rateLimit = require("express-rate-limit");
+
 dotenv.config({ path: "../config/.env" });
 
-const rateLimiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: "middleware",
-  points: 100, // Number of requests
-  duration: 60, // Per 60 seconds
-  blockDuration: 300, // Block for 5 minutes after limit is reached
+const limiter = rateLimit({
+  windowMs: 30 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
 });
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(async (req, res, next) => {
-  try {
-    await rateLimiter.consume(req.ip); // Consume 1 point per request
-    next();
-  } catch (err) {
-    res.status(429).send("Too many requests.");
-  }
-});
+app.use(limiter);
 
 app.use(
   cors({
